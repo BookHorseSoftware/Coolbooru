597fbc4568fd3c95760df45960595c55ebb97bab
Alexandra Frock <im@alexandra.moe>
2017-01-22 01:19:55 -0500

    Update the assembly info

--- Coolbooru/Properties/AssemblyInfo.cs
+++ Coolbooru/Properties/AssemblyInfo.cs
@@ -1,16 +1,15 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Coolbooru")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A Derpibooru client in C#/.NET")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("antigravities")]
 [assembly: AssemblyProduct("Coolbooru")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCopyright("Copyright © 2017 antigravities.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
